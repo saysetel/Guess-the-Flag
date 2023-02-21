a007f4ba2a7d5561d917cfdd02985c44a3425c15
saysetel <saysetel@gmail.com>
2023-02-21 16:36:43 +0100

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Anastasia Kotova on 21.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
